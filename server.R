256d25c28ff96d9823e5c3b2d3b44f4e1fdacba3
dtharvey <harvey@depauw.edu>
2025-09-09 14:52:03 -0400

    updates
    made changes to ui.R and server.R files; added global.R file

--- server.R
+++ server.R
@@ -1,19 +1,5 @@
 # server for response surface optimization application
 
-# load necessary packages
-library(shiny)
-library(shinythemes)
-library(plot3D)
-
-# set color scheme
-palette("Okabe-Ito")
-
-# functions to create response surface values
-model1 = function(a,b){1.68 + 0.24*a + 0.56*b - 0.04*a^2 - 0.04*b^2}
-model2 = function(a,b){4.0 - 0.4*a + 0.08*a*b}
-model3 = function(a,b){3.264 + 1.537*a + 0.5664*b - 0.1505*a^2 - 0.02734*b^2 - 0.05785*a*b}
-model4 = function(a,b){4 + 0.7*a - 0.9*b - 0.08*a^2 + 0.08*b^2}
-
 shinyServer(function(input, output, session){
 
 # code to plot sequential optimization plots 
--- ui.R
+++ ui.R
@@ -1,6 +1,4 @@
 # user interface for response surface optimization application
-library(shiny)
-library(shinythemes)
 
 ui = navbarPage("AC 3.0: Optimization Strategies",
      theme = shinytheme("journal"),
@@ -10,81 +8,89 @@ ui = navbarPage("AC 3.0: Optimization Strategies",
                  href = "style.css") 
      ),
      
+  # introduction
      tabPanel("Introduction",
       fluidRow(
         withMathJax(),
-        column(width = 6,
-          wellPanel(
-            includeHTML("text/introduction.html") 
-      )),
-        column(width = 6, 
-          align = "center",
-          br(),
-          br(),
+          column(width = 6,
+            wellPanel(
+              class = "scrollable-well",
+              div(
+                class = "html-fragment",
+                includeHTML("text/introduction.html")
+              ))),
+          column(width = 6, 
+            align = "center",
+            br(), br(),
           img(src = "badlands.png", width = "100%")
-          )
-      )),
+      ))),
      
-     tabPanel("Sequential",
-              fluidRow(
-                column(width = 6,
-                       wellPanel(
-                         includeHTML("text/activity1.html")
-                       )),
-                column(width = 6,
-                       align = "center",
-                       splitLayout(
-                         selectInput("eqn1","response surface",
-                                     choices = c("example 1","example 2",
-                                                 "example 3","example 4"), 
-                                     selected = "example 1",
-                                     selectize = FALSE, width = "200px"),
-                         sliderInput("a_initial1",
-                                     "factor A: initial value", 
-                                     min = 0, max = 10,
-                                     step = 1, value = 0, round = TRUE, 
-                                     width = "200px", ticks = FALSE),
-                         sliderInput("b_initial1", 
-                                     "Factor B: initial value",
-                                     min = 0, max = 10, step = 1, 
-                                     value = 0, round = TRUE, 
-                                     width = "200px", ticks = FALSE)
+  #first activity   
+  tabPanel("Sequential",
+    fluidRow(
+      column(width = 6,
+        wellPanel(
+          class = "scrollable-well",
+          div(
+            class = "html-fragment",
+            includeHTML("text/activity1.html")
+          ))),
+        column(width = 6,
+          align = "center",
+            splitLayout(
+              selectInput("eqn1","response surface",
+              choices = c("example 1","example 2",
+                          "example 3","example 4"), 
+              selected = "example 1",
+              selectize = FALSE, width = "200px"),
+            sliderInput("a_initial1",
+                        "factor A: initial value", 
+                        min = 0, max = 10,
+                        step = 1, value = 0, round = TRUE, 
+                        width = "200px", ticks = FALSE),
+              sliderInput("b_initial1", 
+                          "Factor B: initial value",
+                           min = 0, max = 10, step = 1, 
+                           value = 0, round = TRUE, 
+                           width = "200px", ticks = FALSE)
+                    ),
+              splitLayout(
+                radioButtons("display1", "display...", 
+                             choices = c("path only", "path & surface"),
+                              selected = "path only", 
+                              inline = FALSE,
+                              width = "100px"),
+                radioButtons("dim1","... in",
+                            choices = c("2D", "3D"),
+                            selected = "2D", inline = FALSE, 
+                            width = "100px"),
+                sliderInput("act1_turn", "rotate the xy plane",
+                            min = -180, max = 180, step = 1, 
+                            value = 45, width = "150px", 
+                            ticks = FALSE),
+                sliderInput("act1_tilt", "tilt z axis",
+                            min = -180, max = 180, step = 1, 
+                            value = 35,
+                                width = "150px", ticks = FALSE),
                        ),
-                       splitLayout(
-                         radioButtons("display1", "display...", 
-                                      choices = c("path only",
-                                                  "path & surface"),
-                                      selected = "path only", 
-                                      inline = FALSE,
-                                      width = "50px"),
-                         radioButtons("dim1","... in",
-                                      choices = c("2D", "3D"),
-                                      selected = "2D", inline = FALSE, 
-                                      width = "50px"),
-                         sliderInput("act1_turn", "rotate the xy plane",
-                                     min = -180, max = 180, step = 1, 
-                                     value = 45, width = "150px", 
-                                     ticks = FALSE),
-                         sliderInput("act1_tilt", "tilt z axis",
-                                     min = -180, max = 180, step = 1, 
-                                     value = 35,
-                                     width = "150px", ticks = FALSE),
-                       ),
-                       plotOutput("act1_plot", height = "580px")
-                )
-              )),
+                       plotOutput("act1_plot", height = "450px")
+                ))),
     
      
+  # second activity
      tabPanel("Steepest Ascent",
       fluidRow(
         column(width = 6,
           wellPanel(
-            includeHTML("text/activity2.html")
-          )),
+            class = "scrollable-well",
+            div(
+              class = "html-fragment",
+              includeHTML("text/activity2.html")
+            ))),
         column(width = 6,
           align = "center",
           splitLayout(
-            selectInput("eqn2","example",
+            selectInput("eqn2","response surface",
                         choices = c("example 1","example 2","example 3",
                                     "example 4"), selected = "example 1",
                         selectize = FALSE, width = "200px"),
@@ -108,105 +114,114 @@ ui = navbarPage("AC 3.0: Optimization Strategies",
                       min = -180, max = 180, step = 1, value = 35,
                       width = "150px", ticks = FALSE),
           ),
-          plotOutput("act2_plot", height = "580px")
-          )
-        )),
+          plotOutput("act2_plot", height = "450px")
+          ))),
      
+  # third activity
      tabPanel("Fixed-Size Simplex",
-              fluidRow(
-                column(width = 6,
-                       wellPanel(
-                         includeHTML("text/activity3.html")
-                       )),
-                column(width = 6,
-                       align = "center",
-                  splitLayout(
-                    img(src = 'simplex-orientation.png',
-                        width = "75px"),
-                     sliderInput("sim_orient",
-                                  "iniital simplex",
-                                  min = 1, max = 6, value = 4,
-                                 ticks = FALSE, width = "150px"),
-                      sliderInput("va","initial factor A",
-                                min = 2, max = 8,
-                                step = 0.5, value = 2,
-                                ticks = FALSE, width = "150px"),
-                      sliderInput("vb", "initial factor B",
-                                min = 2, max = 8, 
-                                step = 0.5, value = 2,
-                                ticks = FALSE, width = "150")
+      fluidRow(
+        column(width = 6,
+          wellPanel(
+            class = "scrollable-well",
+            div(
+              class = "html-fragment",
+              includeHTML("text/activity3.html")
+            ))),
+          column(width = 6,
+                  align = "center",
+            splitLayout(
+              img(src = 'simplex-orientation.png',
+                  width = "85px"),
+               sliderInput("sim_orient",
+                            "iniital orientation",
+                            min = 1, max = 6, value = 4,
+                            ticks = FALSE, width = "150px"),
+                sliderInput("va","initial factor A",
+                            min = 2, max = 8,
+                            step = 0.5, value = 2,
+                            ticks = FALSE, width = "150px"),
+                sliderInput("vb", "initial factor B",
+                            min = 2, max = 8, 
+                            step = 0.5, value = 2,
+                            ticks = FALSE, width = "150")
                        ),
-                      splitLayout(
-                        selectInput("eqn3","example",
-                                    choices = c("example 1","example 2",
-                                                "example 3","example 4"),
-                                    selected = "example 1",
-                                    selectize = FALSE,
-                                    width = "200px"),
+                splitLayout(
+                  selectInput("eqn3","response surface",
+                              choices = c("example 1","example 2",
+                                          "example 3","example 4"),
+                              selected = "example 1",
+                              selectize = FALSE,
+                              width = "200px"),
                         img(src = 'fixedsimplexMovement.png',
                             width = "200px"),
                         
-                        sliderInput("stepsize", "size of step",
-                                    min = 1, max = 2, step = 0.1,
-                                    value = 1, width = "200px", 
-                                    ticks = FALSE)
+                      sliderInput("stepsize", "size of simplex",
+                                   min = 1, max = 2, step = 0.1,
+                                   value = 1, width = "200px", 
+                                  ticks = FALSE)
                        ),
-                       plotOutput("act3_plot", height = "550px")
-                )
-              )),
+                       plotOutput("act3_plot", height = "450px")
+                ))),
      
+  # fourth activity
      tabPanel("Variable-Sized Simplex",
-              fluidRow(
-                column(width = 6,
-                       wellPanel(
-                         includeHTML("text/activity4.html")
-                       )),
-                column(width = 6,
-                       align = "center",
-                       splitLayout(img(src = 'simplex-orientation.png',
-                                       width = "75px"),
-                                   sliderInput("sim_orient4",
-                                               "iniital simplex",
-                                               min = 1, max = 6, value = 4,
-                                               ticks = FALSE, width = "150px"),
-                                   sliderInput("va4","initial factor A",
-                                               min = 1, max = 9,
-                                               step = 0.5, value = 1,
-                                               ticks = FALSE, width = "150px"),
-                                   sliderInput("vb4", "initial factor B",
-                                               min = 1, max = 9, 
-                                               step = 0.5, value = 1,
-                                               ticks = FALSE, width = "150")
+      fluidRow(
+        column(width = 6,
+          wellPanel(
+            class = "scrollable-well",
+            div(
+              class = "html-fragment",
+              includeHTML("text/activity4.html")
+            ))),
+         column(width = 6,
+            align = "center",
+              splitLayout(img(src = 'simplex-orientation.png',
+                              width = "80px"),
+                  sliderInput("sim_orient4",
+                              "iniital orientation",
+                               min = 1, max = 6, value = 4,
+                              ticks = FALSE, width = "150px"),
+                                sliderInput("va4","initial factor A",
+                                      min = 1, max = 9,
+                                      step = 0.5, value = 1,
+                                      ticks = FALSE, width = "150px"),
+                                  sliderInput("vb4", "initial factor B",
+                                      min = 1, max = 9, 
+                                      step = 0.5, value = 1,
+                                      ticks = FALSE, width = "150")
                        ),
-                       splitLayout(
-                         selectInput("eqn4","example",
-                                     choices = c("example 1","example 2",
-                                                 "example 3","example 4"),
-                                     selected = "example 1",
-                                     selectize = FALSE,
-                                     width = "200px"),
-                         img(src = 'simplexMovement.png',
+                  splitLayout(
+                      selectInput("eqn4","response surface",
+                                  choices = c("example 1","example 2",
+                                              "example 3","example 4"),
+                                  selected = "example 1",
+                                  selectize = FALSE,
+                                  width = "200px"),
+                        img(src = 'simplexMovement.png',
                              width = "100%"),
                          sliderInput("iterations4", "number of iterations",
                                      min = 1, max = 30, step = 1,
                                      value = 1,width = "200px", 
                                      ticks = FALSE)
                        ),
-                       plotOutput("act4_plot", height = "550px")
-                )
-              )),
+                       plotOutput("act4_plot", height = "450px")
+                ) )),
      
+  # wrapping up
      tabPanel("Wrapping Up",
-              fluidRow(
-                column(width = 6,
-                  wellPanel(id = "wrapuppanel",
-                      style = "overflow-y:scroll; max-height: 750px",
-                              includeHTML("text/wrapup.html"))),
-                column(width = 6,
-                       align = "center",
-                       plotOutput("wrapup_plot", height = "700px")
+      fluidRow(
+        column(width = 6,
+          wellPanel(
+            class = "scrollable-well",
+            div(
+              class = "html-fragment",
+              includeHTML("text/wrapup.html")
+            ))),
+          column(width = 6,
+                align = "center",
+                plotOutput("wrapup_plot", height = "650px")
                 )
                 
-              )) # close tab panel
+              )) 
 
-     ) # close navbar
+     ) 
